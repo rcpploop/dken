7add24f0ad1e17c5edfc760ef1250639f3dc4508
spn <musicfilestore@gmail.com>
2021-11-21 23:07:36 +0900

    a

--- src/s2have.cpp
+++ src/s2have.cpp
@@ -26,12 +26,46 @@ void print_dataframe(const DataFrame& input){
   Rcout << "\n";
 }
 
+
+
+
 void print_map(const std::unordered_map<int,NumericVector>& map){
   for(auto itr=map.begin(); itr!=map.end(); itr++) {
     Rcout << "key: " << itr->first << ":\t";      // キーを表示
-    Rcout << itr->second << "\n";
+    Rcout << itr->second << "\n" << "\n";
   }
 }
+
+void print_map(const std::unordered_map<int,std::vector<int>>& map){
+  for(auto itr=map.begin(); itr!=map.end(); itr++) {
+    Rcout << "key: " << itr->first << ":\t";      // キーを表示
+    
+    for(int i = 0; i < itr->second.size();i++){
+      Rcout << itr->second[i] << "\t";
+    }
+    Rcout << "\n";
+  }
+  Rcout << "\n" << "\n";
+}
+
+
+std::vector<int> summarise_vector(std::vector<std::vector<int>> vector2d){
+  std::vector<int> output;
+  std::size_t sz = vector2d[0].size();
+  
+  for(auto itr=vector2d.begin(); itr != vector2d.end(); itr++){
+    int s = 0;
+    for(int i = 0; i < sz; ++i){
+      std::vector<int> temp = *itr;
+      s += temp[i];
+    }
+    output.push_back(s);
+  }
+  
+  return output;
+}
+
+
 
 
 std::vector<int> cpp_s2loop(const int s2,const DataFrame& input_fed,
@@ -47,73 +81,97 @@ std::vector<int> cpp_s2loop(const int s2,const DataFrame& input_fed,
   
   const NumericVector s1_col = input_fed[0];
   const NumericVector s2_col = input_fed[1];
-  const int input_col = input_fed.length() -2;
   
   // data_origin in s2
   const LogicalVector bool_s2 = (s2_col == s2);
-
-  // find s2 in s1_col
+  // s2 in s1_col
   const LogicalVector bool_s1 = (s1_col == s2);
   // zero in s2_col
-  LogicalVector bool_0 = (s2_col == 0);
+  const LogicalVector bool_0 = (s2_col == 0);
   // next_s2 == zero
-  LogicalVector bool_s2_0 = bool_s1 & bool_0;
-  // next_s2 not zero
-  LogicalVector bool_s2next = bool_s1 & !(bool_0);
+  const LogicalVector bool_s2_0 = bool_s1 & bool_0;
   // data_origin or s2 == 0
   const LogicalVector bool_s2end = bool_s2|bool_s2_0;
+  // next_s2 not zero
+  const LogicalVector bool_s2next = bool_s1 & !(bool_0);
+
 
   // summarize data_origin or s2==0
   std::vector<int> output;
   
-  for(int i = 0; i < input_col-2 ; i++){
+  const int input_colnum = input_fed.length() -2;
+  
+  for(int i = 0; i < input_colnum ; i++){
     
-    NumericVector input_col = input_fed[i+2];
-    input_col = input_col[bool_s2end];
-    int s = sum(input_col);
+    NumericVector col_tmp = input_fed[i+2];
+    col_tmp = col_tmp[bool_s2end];
+    int s = sum(col_tmp);
     output.push_back(s);
     
   }
 
-  // find next_s2 in s2_col
-  NumericVector next_s2 = s2_col[bool_s2next];
+  // get next_s2 in s2_col
+  const NumericVector next_s2 = s2_col[bool_s2next];
   const NumericVector s2_unique = unique(next_s2);
   
   if(s2_unique.size() == 0){
-    const int input_col = input_fed.length() -2;
     
-    const std::vector<int> output (input_col,0);
+    // const std::vector<int> output (input_column,0);
     memo[s2] = output;
     
     return output;
     
   }else{
   
+  
+  // Analyze s2 in order
   std::vector<std::vector<int>> store; 
-    
+  store.push_back(output);
+  
   for(int i = 0; i < s2_unique.size(); i++){
+    const int s = s2_unique[i];
     
-    if(memo.count(s2_unique[i]) > 0){
-      
-      int s = s2_unique[i];
+    // If s2 exists in the memo, store the value of the memo in s2
+    if(memo.count(s) > 0){
+    
       store.push_back(memo[s]);
       
     }else{
-      // std::vector<int> output_temp = cpp_s2loop();
+      std::vector<int> output_temp = cpp_s2loop(s,input_fed,memo);
+      store.push_back(output_temp);
     }
   }
+    return summarise_vector(store);
   }
-  std::vector<int> x = {1,2,3};
-  return x;
-  
+}
+
+
+
+DataFrame map_to_dataframe(std::unordered_map<int,std::vector<int>>& map){
+    
+    NumericVector col;
+    DataFrame output;
+    
+    for(auto itr=map.begin(); itr!=map.end(); itr++) {
+      col.push_back(itr->first);
+      // col.push_back(itr->second);
+      for(int i = 0; i < itr->second.size(); ++i){
+        col.push_back(itr->second[i]);
+      }
+      
+      const NumericVector col_clone = clone(col);
+      output.push_back(col_clone);
+    }
+    
+    return output;
 }
 
 
 
 // [[Rcpp::export]]
-DataFrame cpp_s2have(const DataFrame input)
+DataFrame cpp_s2has(const DataFrame input)
 {
-  // print_dataframe(input);
+    print_dataframe(input);
     const NumericVector fed_1 = input[0]; // fed_1
     const NumericVector fed_2 = input[1]; // fed_2
     const NumericVector s1 = input[2];      // s1
@@ -142,8 +200,7 @@ DataFrame cpp_s2have(const DataFrame input)
       
       std::unordered_map<int, NumericVector> map_r;
       const int input_col = input.length();
-      // Rcout << "icol" << input_col << "\n";
-      
+
       // add columns
       for(int j = 4; j < input_col; j++){
         NumericVector temp = input[j];
@@ -153,57 +210,48 @@ DataFrame cpp_s2have(const DataFrame input)
         }
       
       // print_map(map_r);
-      // Rcout << "Feder: " << fed_list[i] << "\n"; 
-      // print_dataframe(input_sub);
+      Rcout << "Feder: " << fed_list[i] << "\n"; 
+      print_dataframe(input_sub);
 
       // unique s2_sub and remove 0 or -1
       NumericVector s2_tmp = unique(s2_sub).sort();
       const NumericVector s2_unique = s2_tmp[s2_tmp > 0];
+      Rcout << "s2_unique: " << s2_unique << "\n"; 
 
       for(int k = 0; k < s2_unique.size(); k++){
         if(memo.count(s2_unique[k]) > 0){
           
+          // if memo already has data, skip
           continue;
           
         }else{
           
           std::vector<int> output = cpp_s2loop(s2_unique[k],input_sub,memo);
           memo[s2_unique[k]] = output;
-          
+          print_map(memo);
         }
     }
-    // memo[1] = x;
-    // memo[2] = y;
-    // memo[3] = z;
-
-   // DataFrame temp = {};
-   // for(auto itr = memo.begin(); itr != memo.end(); ++itr) {
-   //    t = clone(itr->second);
-   //   t.push_front(itr->first);
-   //   temp.push_back(t);
-   //  }
     }
-    List zz = List::create("ra");
-
-    return zz;
+    // Convert memo to data frame
+    DataFrame data = map_to_dataframe(memo);
+    return data;
 }
 
 /*** R
 set.seed(100)
 library(tictoc)
-#mat <- data.frame(1:4,2:5,3:6,4:7,5:8,105:108)
-d <- 100
-mat <- data.frame(vapply(runif(d, min = 0, max = 20),round,1),
-                  vapply(runif(d, min = 0, max = 20),round,1),
-                  vapply(runif(d, min = 0, max = 200),round,1),
-                  vapply(runif(d, min = 0, max = 200),round,1),
-                  vapply(runif(d, min = 0, max = 200),round,1),
-                  vapply(runif(d, min = 0, max = 200),round,1),
-                  vapply(runif(d, min = 0, max = 200),round,1))
+mat <- data.frame(1:3,2:4,3:5,4:6,5:7,6:8,101:103)
+# d <- 5
+# mat <- data.frame(vapply(runif(d, min = 0, max = 20),round,1),
+#                   vapply(runif(d, min = 0, max = 20),round,1),
+#                   vapply(runif(d, min = 0, max = 200),round,1),
+#                   vapply(runif(d, min = 0, max = 200),round,1),
+#                   vapply(runif(d, min = 0, max = 200),round,1),
+#                   vapply(runif(d, min = 0, max = 200),round,1),
+#                   vapply(runif(d, min = 0, max = 200),round,1))
 colnames(mat) <- c("fed1","fed2","s1","s2","data1","data2","data3")
 tic()
-k <- cpp_s2have(mat)
+k <- cpp_s2has(mat)
 toc()
-#k
 */
 
